4bced6a1be4c8399b998b4372cbd04f2791aadfe
Marekus <marek.stransky1@gmail.com>
2024-04-17 10:49:53 +0200

    Display pokemons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import PokemonCard from './components/PokemonCard';
+import { fetchPokemons } from './services/pokemonService';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const initFetch = async () => {
+      const results = await fetchPokemons();
+      setPokemons(results);
+    };
+
+    initFetch();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-4 gap-4">
+          {pokemons.map((pokemon, index) => (
+            <PokemonCard key={index + 1} index={index + 1} pokemon={pokemon} />
+          ))}
+        </div>  
+      </div>
     </div>
   );
 }
