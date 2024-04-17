a8fb7ae82c23d355f8f0edc2fc9134e8d1e2e954
Marekus <marek.stransky1@gmail.com>
2024-04-17 10:49:21 +0200

    Fetch pokemons

---
+++ src/services/pokemonService.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { color } from 'chart.js/helpers';
+
+const BASE_URL = "http://pokeapi.co/api/v2/pokemon";
+
+export const fetchPokemons = async (limit = 100) => {
+    try {
+        const response = await axios.get(`${BASE_URL}?limit=${limit}`);
+        const pokemonPromises = response.data.results.map(async (pokemon) => {
+            const pokemonRecord = await axios.get(pokemon.url);
+            const speciesRecord = await axios.get(pokemonRecord.data.species.url);
+            return {
+                name: pokemonRecord.data.name,
+                image: pokemonRecord.data.sprites.front_default,
+                color: speciesRecord.data.color.name
+            };
+        });
+        return await Promise.all(pokemonPromises);
+    } catch (error) {
+        console.log('Failed to fetch pokemons: ', error);
+        return [];
+    }
+}
