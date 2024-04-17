d81d92bc9545da80bbb53758d1b8f523047506e6
Marekus <marek.stransky1@gmail.com>
2024-04-17 10:49:07 +0200

    Pokemon colors

---
+++ src/utils/pokemonColors.js
@@ -0,0 +1,13 @@
+const pokemonColors = {
+    blue: "#5BBCFF",
+    red: "#F0BC8C",
+    green: "#B7FFBE",
+    brown: "#BF9F81",
+    purple: "#D7B2FF",
+    yellow: "#FFF49B",
+    pink: "#FFC4BF",
+    white: "#B7FFBE",
+    gray: "#CFCFC4"
+}
+
+export default pokemonColors;
