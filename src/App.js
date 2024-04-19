0116256d2284aeaea5725104c12e1b6899702277
Marekus <marek.stransky1@gmail.com>
2024-04-19 21:18:28 +0200

    Routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
-import PokemonCard from './components/PokemonCard';
-import { fetchPokemons } from './services/pokemonService';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PokemonList from './components/PokemonList';
+import PokemonDetail from './components/PokemonDetail';
 
 function App() {
-  const [pokemons, setPokemons] = useState([]);
-
-  useEffect(() => {
-    const initFetch = async () => {
-      const results = await fetchPokemons();
-      setPokemons(results);
-    };
-
-    initFetch();
-  }, []);
-
   return (
-    <div className="flex">
-      <div className="container mx-auto">
-        <div className="grid grid-cols-4 gap-4">
-          {pokemons.map((pokemon, index) => (
-            <PokemonCard key={index + 1} index={index + 1} pokemon={pokemon} />
-          ))}
-        </div>  
-      </div>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PokemonList />} />
+        <Route path="/:name" element={<PokemonDetail />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
