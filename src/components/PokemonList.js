47bb1f64487a7f50f34f97dc0d49d59d5692a3be
Marekus <marek.stransky1@gmail.com>
2024-04-20 16:41:49 +0200

    Pokemon list

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import PokemonCard from './PokemonCard';
 import { fetchPokemons } from '../services/pokemonService';
+
+// PrimeReact Components
 import { Paginator } from 'primereact/paginator';
-import { Button } from 'primereact/button';
+import { Skeleton } from 'primereact/skeleton';
 
 function PokemonList() {
     const [pokemons, setPokemons] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const [first, setFirst] = useState(0);
     const [rows, setRows] = useState(20);
 
     useEffect(() => {
         const initFetch = async () => {
-        const results = await fetchPokemons();
-        setPokemons(results);
+            setIsLoading(true);
+            const results = await fetchPokemons();
+            setPokemons(results);
+            setIsLoading(false);
         };
 
         initFetch();
@@ -27,17 +32,29 @@ function PokemonList() {
         <>
             <div className="flex">
                 <div className="container mx-auto py-12">
-                    <h1 className="font-orbitron text-5xl">Pokédex</h1>
+                    <h1 className="font-orbitron text-5xl text-[#0C0C0C]">Pokédex</h1>
                 </div>
             </div>
             <div className="flex">
                 <div className="container mx-auto pb-8">
-                    <div className="grid grid-cols-4 gap-4">
-                    {pokemons.slice(first, first + rows).map((pokemon, index) => (
-                        <PokemonCard key={index} index={first + index + 1} pokemon={pokemon} />
-                    ))}
-                    </div>
-                    <Paginator className="pt-8" first={first} rows={rows} totalRecords={pokemons.length} onPageChange={onPageChange} />
+                    { isLoading ? (
+                        <div className="grid grid-cols-4 gap-4">
+                            {Array.from({length: rows}).map((_, idx) => (
+                                <Skeleton key={idx} shape="rectangle" width="100%" height="117px" className="p-mb-2" />
+                            ))}
+                        </div>
+                    ) : (
+                        <div className="grid grid-cols-4 gap-4">
+                            {pokemons.slice(first, first + rows).map((pokemon, index) => (
+                                <PokemonCard key={index} index={first + index + 1} pokemon={pokemon} />
+                            ))}
+                        </div>
+                    )}
+                    <Paginator className="pt-8" first={first} rows={rows} totalRecords={pokemons.length} onPageChange={onPageChange}
+                    pt={{
+                        pageButton: "",
+                    }}
+                    />
                 </div>
             </div>
         </>
